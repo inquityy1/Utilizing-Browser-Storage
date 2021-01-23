0593e5b62d37fb5a35c0ed6f20bdc2aa80890779
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-01-23 15:16:34 +0100

    working with indexedDB

---
+++ app.js
@@ -0,0 +1,33 @@
+const storeBtn = document.getElementById('store-btn');
+const retrBtn = document.getElementById('retrieve-btn');
+
+const dbRequest = indexedDB.open('StorageDummy', 1);
+
+dbRequest.onupgradeneeded = function(event) {
+	const db = event.target.result;
+	
+	const objStore = db.createObjectStore('products', {keyPath: 'id'});
+
+	objStore.transaction.oncomplete = function(event) {
+		const productsStore = db
+			.transaction('products', 'readwrite')
+			.objectStore('products');
+		productsStore.add({
+			id: 'p1', title: 'A First Product',
+			price: 12.99,
+			tags: ['Expensive', 'Luxury']
+		});
+	};
+};
+
+dbRequest.onerror = function(event) {
+	console.log('ERROR');
+};
+
+storeBtn.addEventListener('click', () => {
+	
+});
+
+retrBtn.addEventListener('click', () => {
+	
+});
